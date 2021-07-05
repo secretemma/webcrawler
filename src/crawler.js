cbe0110ea1dfa6e4ba5b2eaf6c3673d0d6ea5264
Emma Sarachan <emmasarachan@gmail.com>
2021-07-05 14:59:17 -0400

    basic crawler

--- src/crawler.js
+++ src/crawler.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+import { parse } from "node-html-parser";
 
-class Crawler {
+const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+
+export default class Crawler {
   constructor(url) {
     this.urlQueue = [url];
     this.visitedUrls = new Set();
@@ -14,27 +17,57 @@ class Crawler {
   crawl = async () => {
     while (this.urlQueue.length > 0) {
       const urlToFetch = this.urlQueue[0];
-      await this.readPage(urlToFetch);
-      this.urlQueue = this.urlQueue.slice(1);
+      await this.crawlPage(urlToFetch);
     }
     console.log(this.phoneNumbers);
   };
 
-  readPage = async url => {
+  crawlPage = async url => {
+    console.log("Visiting ", url);
+    try {
+      const pageContent = await this.fetchPage(url);
+      const urls = this.getPageUrls(pageContent, url);
+      const dedupedUrls = [...new Set(urls)].filter(
+        url => !this.visitedUrls.has(url)
+      );
+      this.urlQueue = [...this.urlQueue, ...dedupedUrls];
+
+      this.phoneNumbers = [
+        ...this.phoneNumbers,
+        ...this.getPagePhoneNumbers(pageContent)
+      ];
+    } catch (e) {
+      // TODO: handle diff errors differently
+      console.log("Error visiting ", url);
+      console.log(e);
+      return;
+    } finally {
+      this.urlQueue = this.urlQueue.slice(1);
+      this.visitedUrls.add(url);
+    }
+  };
+
+  fetchPage = async url => {
     const response = await axios.get(url);
-    this.visitedUrls.add(url);
+    return response.data;
+  };
+
+  getPageUrls = (pageContent, pageUrl) => {
+    const parsedPage = parse(pageContent);
+    return parsedPage.querySelectorAll("a").map(link => {
+      const urlOrPath = link.getAttribute("href");
+      // TODO: make sure all URL types are handled
+      if (urlRegex.test(urlOrPath)) {
+        return urlOrPath;
+      } else {
+        const [protocol, _, host] = pageUrl.split("/");
+        return `${protocol}//${host}/${urlOrPath}`;
+      }
+    });
+  };
+
+  getPagePhoneNumbers = pageContent => {
+    // TODO: extract phone numbers
+    return [];
   };
 }
-
-const args = process.argv.slice(2);
-if (args.length < 1) {
-  console.log("Must pass a starting URL");
-  process.exit(1);
-}
-
-const startURL = args[0];
-
-// TODO: validate URL
-
-const crawler = new Crawler(startURL);
-crawler.crawl();
--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import "@babel/polyfill";
-import "./crawler";
+
+import Crawler from "./crawler";
+
+const args = process.argv.slice(2);
+if (args.length < 1) {
+  console.log("Must pass a starting URL");
+  process.exit(1);
+}
+
+const startURL = args[0];
+
+// TODO: validate URL
+
+const crawler = new Crawler(startURL);
+crawler.crawl();
